15ee9d51d2919aac3e9c744d97a369b9daf13289
Junx5863 <nicolaszamb99@gmail.com>
2024-05-22 21:48:20 -0500

    Fisrt Commit

--- src/core/db/database.js
+++ src/core/db/database.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDvNolG9-noMGqWTTd12wSGhi97CcUwWU",
-  authDomain: "e-commerce-react-d7d17.firebaseapp.com",
-  projectId: "e-commerce-react-d7d17",
-  storageBucket: "e-commerce-react-d7d17.appspot.com",
-  messagingSenderId: "685381971631",
-  appId: "1:685381971631:web:9bf39bda2a4679b65aae5a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
